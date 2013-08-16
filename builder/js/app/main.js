f32779ef502b271e10c858efda97a2b28f78fad1
pluxxi <pluxxi.shootmania@gmail.com>
2013-08-16 11:11:32 -0500

    oops

--- builder/js/app/main.js
+++ builder/js/app/main.js
@@ -40,7 +40,7 @@ function(
 
 		Globals.initialize();
 
-		if(!Globals.isLtIEVersion(9))
+		if(!Globals.isLtIEVersion(9) && !Globals.IS_MOBILE_DEVICE)
 		{
 			// Bootstrap and JQuery UI both define $.fn.button, so we always want to use JQuery UI's $.fn.button.
 			// Bootstrap always loads after JQuery UI, so this will always work.
@@ -69,12 +69,31 @@ function(
 			ViewManager.views.stepButtons.updateButtonStates();
 
 
-			StepManager.start("#step-content");
+			StepManager.start("#step-global", "#step-content");
 			loadGeneralPolyfills();
-
-			// Hide Loading Gif
-			$("#loading-templates").hide();
 		}
+
+		else if(!Globals.IS_MOBILE_DEVICE)
+		{
+			$("#step-content").append('' +
+			'<div class="not-supported" style="width: 50%; margin-top: 10%;">' +
+                'You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> ' +
+                'or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> in order to use this app.' +
+            '</div>');
+		}
+
+		else
+		{
+			$("#step-content").append('' +
+			'<div class="not-supported" style="margin-top: 10%;">' +
+                'The "CCG Builder" feature of <a href="/">ccgbuilder.com</a> will support <b>mobile</b> devices soon. ' +
+                'We are sincerely sorry for this inconvienience. You can still <a href="#">browse</a> and <a href="#">manage</a> CCGs ' +
+                'on your mobile device, however!' +
+            '</div>');
+		}
+
+		// Hide Loading Gif
+		$("#loading-templates").hide();
 
 		console.log("App initialized!");
 	});
