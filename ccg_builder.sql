cb99bbf13f457eb15ebf193e5a8d3a8354c732b4
pluxxi <pluxxi.shootmania@gmail.com>
2013-08-18 17:28:12 -0500

    updated sql

--- ccg_builder.sql
+++ ccg_builder.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 29, 2013 at 11:06 PM
+-- Generation Time: Aug 18, 2013 at 10:27 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `ccgs` (
   `name` varchar(30) NOT NULL,
   `base_path` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
 
 --
 -- Dumping data for table `ccgs`
@@ -40,6 +40,25 @@ CREATE TABLE IF NOT EXISTS `ccgs` (
 
 INSERT INTO `ccgs` (`id`, `author`, `name`, `base_path`) VALUES
 (10, 'pluxxi', 'test', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `components`
+--
+
+CREATE TABLE IF NOT EXISTS `components` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ccgid` int(11) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `type` tinyint(1) NOT NULL,
+  `layer` mediumint(3) NOT NULL,
+  `width` mediumint(3) NOT NULL,
+  `height` mediumint(3) NOT NULL,
+  `posX` int(4) NOT NULL,
+  `posY` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
